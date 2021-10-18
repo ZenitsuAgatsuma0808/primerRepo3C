7948e58d374f16f0d9f589cd10b2dde4782e90fb
ALEJANDRO GARCIA <90258740+ZenitsuAgatsuma0808@users.noreply.github.com>
2021-10-18 17:30:55 +0000

    Terminamos Cuarto_rectangular.py

--- Cuarto_rectangular.py
+++ Cuarto_rectangular.py
@@ -6,4 +6,22 @@ factor_conversion = 10.764
 # 4. Imprimir "Cual es el largo del cuarto en metros?"
 print ("Cual es el largo del cuarto en metros?")
 # 5. Hacer largo = entrada del usuario 
-largo = input () # convertir a numero 
+largo = float(input())
+6. # Imprimir "Cual es el ancho de cuarto en metros?"
+print ("Cual es el ancho del cuarto en metros?") 
+# 7. Hacer ancho = entrada del usuario
+ancho = float(input())
+# 8. Hacer area_metros = largo * ancho
+area_metros = largo * ancho
+# 9. Hacer area_pies = factor_conversion * area_metros 
+area_pies = factor_conversion * area_metros
+
+# 10. Imprimir "Ingresaste las siguinetes dimensiones: ", ancho, " x ", largo, " metros."
+print ("Ingresaste las siguientes dimensiones: ", ancho, " x ", largo, " metros") 
+# 11. Imprimir el área es de:"
+print ("El area es de: ")
+# 12. Imprimir area_metros, " metros cuadrados. "
+print(area_metros, " metros cuadrados")
+# 13. Imprimir area_pies " pies_cuadrados, "
+print (area_pies, " pies cuadrados")
+# 14. Fin
