b9a0a332dd15c7101e861f8b099753e18c94bf2c
ZenitsuAgatsuma0808 <90258740+ZenitsuAgatsuma0808@users.noreply.github.com>
2021-10-19 07:37:45 -0500

    Create Pizza_party.py

---
+++ Pizza_party.py
@@ -0,0 +1,13 @@
+Pizza_party.py
+Inicio
+Preguntar "¿Cuantas personas habrá en la fiesta?"
+20
+Preguntar "¿Cuantas pizzas habrán?"
+10
+Preguntar "¿Cuantas rebanadas tendrá cada pizza?
+10
+Multiplicar 10 * 10 (las pizzas por la cantidad de rebanadas que tiene cada pizza)
+100 = rebanadas totales
+Número de rebanadas que le tocan a cada quien = 100 (reabanadas totales) / 20 (personas) 
+5 = Número de rebanadas que le tocará a cada persona
+FIN
